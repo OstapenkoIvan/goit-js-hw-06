ecd5e666ccf6f3102e06eb72e1fef8f2692a85d8
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-08-13 12:32:37 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const backgroundEl = document.querySelector(".widget");
+const colorEl = document.querySelector(".color");
+const buttonActive = document.querySelector(".change-color");
+
+buttonActive.addEventListener("click", changeBg);
+
+function changeBg(event) {
+  const currentColor = getRandomHexColor();
+  backgroundEl.style.backgroundColor = currentColor;
+  colorEl.textContent = currentColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
