2a8143f7a61daf0a16ae2aa2f566160e1851b889
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-08-13 13:37:14 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesContainer = document.querySelector("#boxes");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+let markupToPrint = "";
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(event) {
+  boxesContainer.innerHTML = "";
+  const generatedNumber = document.querySelector("input").value;
+  for (let i = 1; i <= generatedNumber; i++) {
+    const boxSize = i * 10 + 20 + "px";
+
+    const createBox = document.createElement("div");
+    console.log(createBox);
+    createBox.style.backgroundColor = getRandomHexColor();
+    createBox.style.width = boxSize;
+    createBox.style.height = boxSize;
+
+    boxesContainer.append(createBox);
+  }
+}
+
+function destroyBoxes(event) {
+  boxesContainer.innerHTML = "";
+}
